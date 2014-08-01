ed52f5508e19f91c2ca193d271e9a321e39f3fb2
Matt Davis <jiffyclub@gmail.com>
2014-08-01 14:49:24 -0700

    ifdef out some DLLExport stuff so it only shows up on Windows.

--- pyaccess/shared.h
+++ pyaccess/shared.h
@@ -1,11 +1,11 @@
 #pragma once
-#ifdef LINUX
-#define DLLImport
-#define DLLExport 
-#define DOTIMER 0
+#ifdef WIN32
+    #define DLLImport __declspec(dllimport)
+    #define DLLExport __declspec(dllexport)
+    #define DOTIMER 0
 #else
-#define DLLImport __declspec(dllimport)
-#define DLLExport __declspec(dllexport)
-#define DOTIMER 0
+    #define DLLImport
+    #define DLLExport
+    #define DOTIMER 0
 #endif
 #define FILE_LOG(logINFO) (std::cout)
