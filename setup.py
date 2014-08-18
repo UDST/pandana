81624b2321195e3a02ee4ab1d3b6d200c25928fe
Matt Davis <jiffyclub@gmail.com>
2014-08-18 14:06:05 -0700

    Add test command to setup.py
    This requires regular, absolute imports in the tests.

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-from setuptools import setup, Extension
+import sys
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.test import test as TestCommand
 import numpy as np
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args or '')
+        sys.exit(errno)
 
 extension_name = '_pyaccess'
 extension_version = '0.1dev'
@@ -7,31 +29,31 @@ extension_version = '0.1dev'
 include_dirs = [
     np.get_include(),
     '.',
-    'ann_1.1.2/include'
+    'src/ann_1.1.2/include'
 ]
 
-packages = ['pyaccess']
+packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
 source_files = [
-    'pyaccess/accessibility.cpp',
-    'pyaccess/graphalg.cpp',
-    'pyaccess/nearestneighbor.cpp',
-    'pyaccess/pyaccesswrap.cpp',
-    'contraction_hierarchies/src/libch.cpp',
-    'ann_1.1.2/src/ANN.cpp',
-    'ann_1.1.2/src/brute.cpp',
-    'ann_1.1.2/src/kd_tree.cpp',
-    'ann_1.1.2/src/kd_util.cpp',
-    'ann_1.1.2/src/kd_split.cpp',
-    'ann_1.1.2/src/kd_dump.cpp',
-    'ann_1.1.2/src/kd_search.cpp',
-    'ann_1.1.2/src/kd_pr_search.cpp',
-    'ann_1.1.2/src/kd_fix_rad_search.cpp',
-    'ann_1.1.2/src/bd_tree.cpp',
-    'ann_1.1.2/src/bd_search.cpp',
-    'ann_1.1.2/src/bd_pr_search.cpp',
-    'ann_1.1.2/src/bd_fix_rad_search.cpp',
-    'ann_1.1.2/src/perf.cpp'
+    'src/accessibility.cpp',
+    'src/graphalg.cpp',
+    'src/nearestneighbor.cpp',
+    'src/pyaccesswrap.cpp',
+    'src/contraction_hierarchies/src/libch.cpp',
+    'src/ann_1.1.2/src/ANN.cpp',
+    'src/ann_1.1.2/src/brute.cpp',
+    'src/ann_1.1.2/src/kd_tree.cpp',
+    'src/ann_1.1.2/src/kd_util.cpp',
+    'src/ann_1.1.2/src/kd_split.cpp',
+    'src/ann_1.1.2/src/kd_dump.cpp',
+    'src/ann_1.1.2/src/kd_search.cpp',
+    'src/ann_1.1.2/src/kd_pr_search.cpp',
+    'src/ann_1.1.2/src/kd_fix_rad_search.cpp',
+    'src/ann_1.1.2/src/bd_tree.cpp',
+    'src/ann_1.1.2/src/bd_search.cpp',
+    'src/ann_1.1.2/src/bd_pr_search.cpp',
+    'src/ann_1.1.2/src/bd_fix_rad_search.cpp',
+    'src/ann_1.1.2/src/perf.cpp'
 ]
 
 extra_compile_args = [
@@ -47,11 +69,8 @@ extra_compile_args = [
     '-fopenmp'
 ]
 
-py_modules = ['pyaccess/pyaccess', 'pyaccess/urbanaccess']
-
 setup(
     packages=packages,
-    py_modules=py_modules,
     name='pyaccess',
     version=extension_version,
     ext_modules=[
@@ -61,5 +80,13 @@ setup(
             include_dirs=include_dirs,
             extra_compile_args=extra_compile_args
         )
-    ]
+    ],
+    install_requires=[
+        'brewer2mpl>=1.4',
+        'matplotlib>=1.3.1',
+        'numpy>=1.8.0',
+        'pandas>=0.13.1',
+    ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest}
 )
