07f86d5720dc71f42337da5a76f5eec19fbf3725
Matt Davis <jiffyclub@gmail.com>
2014-08-25 15:09:46 -0700

    Set default setup.py test args back to nothing.

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ class PyTest(TestCommand):
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = ['--cov-report', 'term-missing', '--cov',
-                            '.']
-        # self.pytest_args = None
+        self.pytest_args = None
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
