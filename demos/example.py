fa62d27d9eaaf3d0fb84853ef937be0a0fadc2dc
Matt Davis <jiffyclub@gmail.com>
2014-08-28 12:28:12 -0700

    Get an absolute path to the test data location.

--- demos/example.py
+++ demos/example.py
@@ -1,8 +1,12 @@
-import pandas as pd
+import os.path
 
+import pandas as pd
 import pandana.network as pdna
 
-store = pd.HDFStore('../pandana/tests/osm_sample.h5', "r")
+storef = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    '../pandana/tests/osm_sample.h5')
+store = pd.HDFStore(storef, "r")
 nodes, edges = store.nodes, store.edges
 net = pdna.Network(nodes.x, nodes.y, edges["from"], edges.to,
                    edges[["weight"]])
