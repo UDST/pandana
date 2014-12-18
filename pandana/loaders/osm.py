99ebdfa454170685e989f36b99fb90406a36340b
Matt Davis <jiffyclub@gmail.com>
2014-12-18 11:11:05 -0800

    Functions for processing OSM queries into DataFrames
    Can get DataFrames of nodes, ways, and waynodes.
    Waynodes are essentially a join index for nodes and ways,
    though it's a DataFrame for consistency with the others.

--- pandana/loaders/osm.py
+++ pandana/loaders/osm.py
@@ -2,7 +2,19 @@
 Tools for creating Pandana networks from Open Street Map.
 
 """
+import pandas as pd
 import requests
+
+uninteresting_tags = {
+    'source',
+    'source_ref',
+    'source:ref',
+    'history',
+    'attribution',
+    'created_by',
+    'tiger:tlid',
+    'tiger:upload_uuid',
+}
 
 
 def osm_query(lat_min, lng_min, lat_max, lng_max):
@@ -50,3 +62,94 @@ def make_osm_query(lat_min, lng_min, lat_max, lng_max):
 
     return req.json()
 
+
+def process_node(e):
+    """
+    Process a node element entry into a dict suitable for going into
+    a Pandas DataFrame.
+
+    Parameters
+    ----------
+    e : dict
+
+    Returns
+    -------
+    node : dict
+
+    """
+    node = {
+        'id': e['id'],
+        'lat': e['lat'],
+        'lon': e['lon']
+    }
+
+    if 'tags' in e:
+        for t, v in e['tags'].items():
+            if t not in uninteresting_tags:
+                node[t] = v
+
+    return node
+
+
+def process_way(e):
+    """
+    Process a way element entry into a list of dicts suitable for going into
+    a Pandas DataFrame.
+
+    Parameters
+    ----------
+    e : dict
+
+    Returns
+    -------
+    way : dict
+    waynodes : list of dict
+
+    """
+    way = {
+        'id': e['id']
+    }
+
+    if 'tags' in e:
+        for t, v in e['tags'].items():
+            if t not in uninteresting_tags:
+                way[t] = v
+
+    waynodes = []
+
+    for n in e['nodes']:
+        waynodes.append({'way_id': e['id'], 'node_id': n})
+
+    return way, waynodes
+
+
+def parse_osm_query(data):
+    """
+    Convert OSM query data to DataFrames of ways and way-nodes.
+
+    Parameters
+    ----------
+    data : dict
+        Result of an OSM query.
+
+    Returns
+    -------
+    nodes, ways, waynodes : pandas.DataFrame
+
+    """
+    nodes = []
+    ways = []
+    waynodes = []
+
+    for e in data['elements']:
+        if e['type'] == 'node':
+            nodes.append(process_node(e))
+        elif e['type'] == 'way':
+            w, wn = process_way(e)
+            ways.append(w)
+            waynodes.extend(wn)
+
+    return (
+        pd.DataFrame.from_records(nodes, index='id'),
+        pd.DataFrame.from_records(ways, index='id'),
+        pd.DataFrame.from_records(waynodes, index='way_id'))
--- pandana/loaders/tests/test_osm.py
+++ pandana/loaders/tests/test_osm.py
@@ -3,14 +3,88 @@ import pytest
 from pandana.loaders import osm
 
 
-@pytest.fixture
+@pytest.fixture(scope='module')
 def bbox():
     return 37.8659303546, -122.2588003879, 37.8661598571, -122.2585062512
 
 
-def test_make_osm_query(bbox):
-    data = osm.make_osm_query(*bbox)
-    assert isinstance(data, dict)
-    assert len(data['elements']) == 37
-    assert len([e for e in data['elements'] if e['type'] == 'node']) == 33
-    assert len([e for e in data['elements'] if e['type'] == 'way']) == 4
+@pytest.fixture(scope='module')
+def query_data(bbox):
+    return osm.make_osm_query(*bbox)
+
+
+def test_make_osm_query(query_data):
+    assert isinstance(query_data, dict)
+    assert len(query_data['elements']) == 37
+    assert len(
+        [e for e in query_data['elements'] if e['type'] == 'node']) == 33
+    assert len([e for e in query_data['elements'] if e['type'] == 'way']) == 4
+
+
+def test_process_node():
+    test_node = {
+        'id': 'id',
+        'lat': 'lat',
+        'lon': 'lon',
+        'extra': 'extra'
+    }
+
+    expected = {
+        'id': 'id',
+        'lat': 'lat',
+        'lon': 'lon'
+    }
+
+    assert osm.process_node(test_node) == expected
+
+    test_node['tags'] = {'highway': 'highway', 'source': 'source'}
+    expected['highway'] = 'highway'
+
+    assert osm.process_node(test_node) == expected
+
+
+def test_process_way():
+    test_way = {
+        "type": "way",
+        "id": 188434143,
+        "timestamp": "2014-01-04T22:18:14Z",
+        "version": 2,
+        "changeset": 19814115,
+        "user": "dchiles",
+        "uid": 153669,
+        "nodes": [
+            53020977,
+            53041093,
+        ],
+        "tags": {
+            'source': 'source',
+            "addr:city": "Berkeley",
+            "highway": "secondary",
+            "name": "Telegraph Avenue",
+        }
+    }
+
+    expected_way = {
+        'id': test_way['id'],
+        'addr:city': test_way['tags']['addr:city'],
+        'highway': test_way['tags']['highway'],
+        'name': test_way['tags']['name']
+    }
+
+    expected_waynodes = [
+        {'way_id': test_way['id'], 'node_id': test_way['nodes'][0]},
+        {'way_id': test_way['id'], 'node_id': test_way['nodes'][1]}
+    ]
+
+    way, waynodes = osm.process_way(test_way)
+
+    assert way == expected_way
+    assert waynodes == expected_waynodes
+
+
+def test_parse_osm_query(query_data):
+    nodes, ways, waynodes = osm.parse_osm_query(query_data)
+
+    assert len(nodes) == 33
+    assert len(ways) == 4
+    assert len(waynodes.index.unique()) == 4
