83100fe5b8afb570b684d180f2831cd1ea7d8df6
fscottfoti <fscottfoti@gmail.com>
2017-05-23 13:00:47 -0700

    cpplint

--- src/graphalg.h
+++ src/graphalg.h
@@ -1,51 +1,54 @@
 #pragma once
 
-#include "shared.h"
 #include <vector>
-#include "contraction_hierarchies/src/libch.h"
+#include <map>
+#include <utility>
+#include "./shared.h"
+#include "./contraction_hierarchies/src/libch.h"
 
 typedef unsigned int NodeID;
 
 #define DISTANCEMULTFACT 1000.0
 
 namespace MTC {
-	namespace accessibility {
+namespace accessibility {
 
-		typedef std::map<int,float> DistanceMap;
-		typedef std::vector<std::pair<NodeID, float> > DistanceVec;
-		
-		class Graphalg
-		{
-		public:
+typedef std::map<int, float> DistanceMap;
+typedef std::vector<std::pair<NodeID, float> > DistanceVec;
 
-		    void DLLExport Build(int *nodeids, float *nodexy, int numnodes,
-							int *edges, float *edgeweights, int numedges,
-							bool twoway);
+class Graphalg {
+ public:
+  void DLLExport Build(
+    int *nodeids, float *nodexy, int numnodes,
+    int *edges, float *edgeweights, int numedges,
+    bool twoway);
 
-			void DLLExport BuildNN(std::vector<float> x, std::vector<float> y);
-			
-			int DLLExport NearestNode(float x, float y, double *distance);
+  void DLLExport BuildNN(std::vector<float> x, std::vector<float> y);
 
-			std::vector<NodeID> Route(int src, int tgt, int threadNum=0);
-		    double Distance(int src, int tgt, int threadNum=0);
+  int DLLExport NearestNode(float x, float y, double *distance);
 
-			void Range(int src, double maxdist, int threadNum, 
-					DistanceVec &ResultingNodes);
+  std::vector<NodeID> Route(int src, int tgt, int threadNum = 0);
 
-			DistanceMap NearestPOI(int category, int src, double maxdist, 
-												int number, int threadNum=0);
+  double Distance(int src, int tgt, int threadNum = 0);
 
-			void initPOIs(int numcategories, double maxdist, int maxitems) {
-				ch.createPOIIndexArray(numcategories, maxdist*DISTANCEMULTFACT,
-									   maxitems);
-			}
-			void addPOIToIndex(int category, int i) {
-				ch.addPOIToIndex(category,i);
-			}
+  void Range(int src, double maxdist, int threadNum,
+             DistanceVec &ResultingNodes);
 
-			int numnodes;
+  DistanceMap NearestPOI(
+    int category, int src, double maxdist,
+    int number, int threadNum = 0);
 
-			CH::ContractionHierarchies ch;
-		};
-	}
-}
+  void initPOIs(int numcategories, double maxdist, int maxitems) {
+    ch.createPOIIndexArray(numcategories, maxdist*DISTANCEMULTFACT,
+                 maxitems);
+  }
+
+  void addPOIToIndex(int category, int i) {
+    ch.addPOIToIndex(category, i);
+  }
+
+  int numnodes;
+  CH::ContractionHierarchies ch;
+};
+}  // namespace accessibility
+}  // namespace MTC
