87e79a3597755fff79daf5d392e527cf40d72fcc
Sam Maurer <maurer@berkeley.edu>
2020-11-17 12:23:24 -0800

    Versioning

--- pandana/__init__.py
+++ pandana/__init__.py
@@ -1,3 +1,3 @@
 from .network import Network
 
-version = __version__ = '0.5.1.dev0'
+version = __version__ = '0.6.dev0'
--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ cyaccess = Extension(
 ## Standard setup
 ###############################################
 
-version = '0.5.1.dev0'
+version = '0.6.dev0'
 
 packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
 
