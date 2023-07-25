9616c2c31c870163a36c333022eb76d65170d275
Federico Catalano <44501803+PyMap@users.noreply.github.com>
2023-07-25 18:44:33 -0300

    Informative warning for shortest path unsigned integer (#169)

--- pandana/utils.py
+++ pandana/utils.py
@@ -25,3 +25,10 @@ def reindex(series1, series2):
                   right_index=True,
                   how="left")
     return df.right
+
+def adjacency_matrix(edges_df, plot_matrix=False):
+    df = pd.crosstab(edges_df['from'], edges_df['to'])
+    idx = df.columns.union(df.index)
+    df = df.reindex(index = idx, columns=idx, fill_value=0)
+
+    return df
